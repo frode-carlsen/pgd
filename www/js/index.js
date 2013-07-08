d2f0353ae07cf4f48ef23df24f8d73bb33168e8a
frodecarlsen <fc@steria.no>
2013-07-08 19:38:06 +0200

    initial map

--- www/js/index.js
+++ www/js/index.js
@@ -25,7 +25,25 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+    },
+    
+    // setup map
+    initMap: function() {
+    	
+    	// create a map in the "map" div, set the view to a given place and zoom
+    	var map = L.map('map').setView([51.505, -0.09], 13);
 
-        console.log('Received Event: ' + id);
+    	// add an OpenStreetMap tile layer
+    	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    	}).addTo(map);
+
+    	// add a marker in the given location, attach some popup content to it and open the popup
+    	L.marker([51.5, -0.09]).addTo(map)
+    	    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+    	    .openPopup();
+    	
+    	
     }
+    
 };
