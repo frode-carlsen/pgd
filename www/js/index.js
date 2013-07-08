908d7b9c5da3537962f793bcdb6aa0080f553def
frodecarlsen <fc@steria.no>
2013-07-08 19:47:33 +0200

    map test

--- www/js/index.js
+++ www/js/index.js
@@ -19,12 +19,13 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    	switch (id) {
+		case "deviceready":
+			app.initMap();
+			break;
+		default:
+			break;
+		}
     },
     
     // setup map
