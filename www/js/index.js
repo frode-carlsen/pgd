5f42b00a39fb348154ef0ab99f00618609ec9b82
frodecarlsen <fc@steria.no>
2013-07-09 09:23:51 +0200

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -27,7 +27,7 @@ var app = {
     	
     	// create a map in the "map" div, set the view to a given place and zoom
     	var map = L.map('map').setView([51.505, -0.09], 13);
-
+    	document.getElementById(id).style.height = window.innerHeight * 0.9;
     	// add an OpenStreetMap tile layer
     	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
